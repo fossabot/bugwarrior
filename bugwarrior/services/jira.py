4272c4d326ac0efd58ff8ebf9375b92ee0bc647b
Kevin Fong <kmf78@georgetown.edu>
2017-05-27 17:26:04 -0700

    [JIRA] Add jiraissuetype UDA

--- bugwarrior/services/jira.py
+++ bugwarrior/services/jira.py
@@ -52,6 +52,7 @@ def _parse_sprint_string(sprint):
     return dict([entry.split('=') for entry in entries])
 
 class JiraIssue(Issue):
+    ISSUE_TYPE = 'jiraissuetype'
     SUMMARY = 'jirasummary'
     URL = 'jiraurl'
     FOREIGN_ID = 'jiraid'
@@ -63,6 +64,10 @@ class JiraIssue(Issue):
     DUE_BY = 'jiraduedate'
 
     UDAS = {
+        ISSUE_TYPE: {
+            'type': 'string',
+            'label': 'Issue Type'
+        },
         SUMMARY: {
             'type': 'string',
             'label': 'Jira Summary'
@@ -124,6 +129,7 @@ class JiraIssue(Issue):
             'entry': self.get_entry(),
             'due': self.get_duedate(),
 
+            self.ISSUE_TYPE: self.get_issue_type(),
             self.URL: self.get_url(),
             self.FOREIGN_ID: self.record['key'],
             self.DESCRIPTION: self.record.get('fields', {}).get('description'),
@@ -247,6 +253,8 @@ class JiraIssue(Issue):
     def get_status(self):
         return self.record['fields']['status']['name']
 
+    def get_issue_type(self):
+        return self.record['fields']['issuetype']['name']
 
 class JiraService(IssueService):
     ISSUE_CLASS = JiraIssue
